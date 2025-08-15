58c4d80ba41de93b6828a66a1fe07f2499f913a4
sange <sange@hp>
2025-08-15 21:17:58 +0530

    Commit

---
+++ src/day3/Encapsulation/Encapsulation.java
@@ -0,0 +1,16 @@
+package day3.Encapsulation;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		//object creation 
+				OopsConcept obj = new OopsConcept();
+				obj.setSerialNum(101);
+				obj.setName("Pooja");
+				obj.setAge(20);
+				System.out.println(obj);
+				
+
+	}
+
+}
